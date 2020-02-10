76088f6f7cf94632b06f15f47f62bb1eca75201b
Ed Chen <aedyucheng@gmail.com>
2020-02-10 11:42:17 +0800

    Create react-axio

---
+++ src/axios.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com'
+});
+
+instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
+
+instance.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+}, error => {
+    return Promise.reject(error);
+});
+
+instance.interceptors.response.use(response => {
+    return response;
+}, error => {
+    return Promise.reject(error);
+});
+
+export default instance;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import axios from 'axios';
+
+// Set axios default from index.js
+// axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+// axios.defaults.headers.common['Authorization'] ='AUTH TOKEN';
+// axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+    return request;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
